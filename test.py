b6a4ecfba090ce307eecfcca3d0d40ec004ea6a4
Andrew Crerar <ascrerar@ncsu.edu>
2016-10-03 07:56:00 -0400

    Converted tests to py3.x, removed newlines from charikar, adding spacing where appropriate 😍

--- test.py
+++ test.py
@@ -1,37 +1,42 @@
-from networkx import *
+import networkx as nx
 import charikar as ch
 import time
 
 
 G = nx.read_gml('test.gml')
-Gn, Gm =  G.number_of_nodes() , G.number_of_edges()
-print 'graph stats:'
-print '# nodes', Gn
-print '# edges', Gm
-print 'avg degree', 2.0*Gm/Gn
+Gn, Gm = G.number_of_nodes(), G.number_of_edges()
 
-print '-------'
-print 'Charikar with dicts:'
+print('graph stats:')
+print('# nodes', Gn)
+print('# edges', Gm)
+print('avg degree {}'.format(2.0 * (Gm / Gn)))
+
+print('-------')
+print('Charikar with dicts:')
+
 tic = time.clock()
 S, avg = ch.charikarDicts(G)
 toc = time.clock()
-print 'time', toc-tic
-print 'Average degree', avg
-Sn, Sm =  S.number_of_nodes() , S.number_of_edges()
-print 'Dense subgraph stats:'
-print '# nodes', Sn
-print '# edges', Sm
-print 'avg degree', 2.0*Sm/Sn
+print('time {}'.format(toc - tic))
+print('Average degree {}'.format(avg))
 
-print '-------'
-print 'Charikar with heap:'
+Sn, Sm = S.number_of_nodes(), S.number_of_edges()
+print('Dense subgraph stats:')
+print('# nodes {}'.format(Sn))
+print('# edges {}'.format(Sm))
+print('avg degree {}'.format(2.0 * (Sm / Sn)))
+
+print('-------')
+print('Charikar with heap:')
+
 tic = time.clock()
 S, avg = ch.charikarHeap(G)
 toc = time.clock()
-print 'time', toc-tic
-print 'Average degree',avg
-Sn, Sm =  S.number_of_nodes() , S.number_of_edges()
-print 'Dense subgraph stats:'
-print '# nodes', Sn
-print '# edges', Sm
-print 'avg degree', 2.0*Sm/Sn
+print('time {}'.format(toc - tic))
+print('Average degree {}'.format(avg))
+
+Sn, Sm = S.number_of_nodes(), S.number_of_edges()
+print('Dense subgraph stats:')
+print('# nodes {}'.format(Sn))
+print('# edges {}'.format(Sm))
+print('avg degree {}'.format(2.0 * (Sm / Sn)))
